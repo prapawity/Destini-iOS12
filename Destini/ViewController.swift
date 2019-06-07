9a0a52d592baabac144b82b54313ff25419f029e
prapawity <60070045@kmitl.ac.th>
2019-06-07 15:35:26 +0700

    ez destiny

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
     let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
     
-    
+    var state = 1
     // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
@@ -41,7 +41,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUI()
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
@@ -50,16 +50,54 @@ class ViewController: UIViewController {
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
-        
-    
+        checkAns(ans: sender.tag)
+        setUI()
+        print(state)
     }
+    func setUI(){
+        switch state {
+        case 1:
+            storyTextView.text = story1
+            topButton.setTitle(answer1a, for: .normal)
+            bottomButton.setTitle(answer1b, for: .normal)
+        case 2:
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+        case 3:
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+        case 4...6:
+            storyTextView.text = state == 4 ? story4 : state == 5 ? story5 : story6
+            topButton.isHidden = true
+            bottomButton.setTitle("Restart", for: .normal)
+            
+        default:
+            state = 1
+            topButton.isHidden = false
+            storyTextView.text = story1
+            topButton.setTitle(answer1a, for: .normal)
+            bottomButton.setTitle(answer1b, for: .normal)
+        }
     
 
-
+    }
+    func checkAns(ans: Int){
+        switch state {
+        case 1:
+            state = chkBtn(btn: ans) == 1 ? 2:3
+        case 2:
+            state = chkBtn(btn: ans) == 1 ? 3:4
+        case 3:
+            state = chkBtn(btn: ans) == 1 ? 6:5
+        default:
+            state = 7
+        }
+    }
+    func chkBtn(btn: Int) -> Int{
+        return btn == 1 ? 1:2
+    }
 
 }
 
